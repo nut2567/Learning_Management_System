8613a5bac124c9a731f6a2c5e05a57ae8d383893
nut2567 <nutmakchine.n@gmail.com>
2024-11-14 03:09:42 +0700

    MONGODB_URI mongoose fix bug

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,11 +2,13 @@ import mongoose  from "mongoose";
 
 export async function connetMongoDB() {
     try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined");
+        }
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected");
-        
-    } catch (error) {
-        console.log("Error connect mongoDB",error);        
-    }
 
+    } catch (error) {
+        console.log("Error connect mongoDB", error);
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 
+import { Button } from "@/app/components/ui/button";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Button className="flex items-center gap-2" size={"sm"}>
+          <svg
+            className="h-3 w-3"
+            viewBox="0 0 76 65"
+            fill="hsl(var(--background)/1)"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="inherit" />
+          </svg>
+          <span>Deploy to Vercel</span>
+        </Button>
+      </main>
     </div>
   );
 }
