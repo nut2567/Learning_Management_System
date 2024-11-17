b7ae7d51193411fd3507feec4fc06d5400f0d6c9
nut2567 <nutmakchine.n@gmail.com>
2024-11-17 08:29:46 +0700

    add image user and api user

--- models/schema.ts
+++ models/schema.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-const useSchema = new Schema({
+const Courses_Schema = new Schema({
     Course_Title: String,
     Instructor_Name: String,
     Course_Duration: Number,
@@ -11,6 +11,24 @@ const useSchema = new Schema({
         default: Date.now
     }, Status: String,
     image: String,
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
-const Courses =   mongoose.models.Courses || mongoose.model("Courses", useSchema);
+const Courses =   mongoose.models.Courses || mongoose.model("Courses", Courses_Schema);
 export default Courses;
+
+const userSchema = new Schema({
+    Instructor_Name: String,
+    age: Number,
+    email : String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }, 
+    image: String,
+    phone: String,
+});
+const User =   mongoose.models.User || mongoose.model("User", userSchema);
+export { User };
